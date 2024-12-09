ee5a9f411d37e99d038b5c435a62bed8b6f85152
Melanie Martin <melmarti@made-f0Ar1s3.clusters.42paris.fr>
2024-12-09 18:24:52 +0100

    update

--- Day_04/ex00/main.c
+++ Day_04/ex00/main.c
@@ -1,21 +1,27 @@
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 16000000UL
 
-ISR (PCINT0_vect)
+ISR(INT0_vect)
 {
-
+	
+	PORTB ^= (1 << PB1);
 }
 
 int	main(void)
 {
-	DDRD &= ~(0 << PB2);
+	DDRD &= ~(1 << PB2);
+	DDRB |= (1 << PB1);
+	PORTB |= (0 << PB1);
 	SREG |= (1 << 7);
 
 	EIMSK |= (1 << INT0);
 
 	
 	while (1)
-		;
+	{
+	}
 	return (0);
 }
